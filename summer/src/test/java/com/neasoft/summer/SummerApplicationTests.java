f1e9845bf5d245f0e4eb9c9255f0c7f21d3e1fa1
TomAndLiu <1134593146@qq.com>
2019-11-08 13:34:59 +0800

    这是我第一次提交

---
+++ summer/src/test/java/com/neasoft/summer/SummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neasoft.summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
